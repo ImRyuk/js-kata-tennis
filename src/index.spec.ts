c2d0d7391612992d3685a0ea0b1b436a032e2882
Clément <jacquelet.clement@outlook.fr>
2021-12-16 16:26:29 +0100

    Adding classes and tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,122 @@
-import { systemUnderTest } from ".";
+import {Game} from "./index"
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+/*describe("Test of displayScore()", function() {
+  it("Should render 15", function() {
+    expect(scoreToDisplay()).toEqual("love");
   });
 });
+
+describe("Test of displayScore()", function() {
+  it("Should render 30", function() {
+    expect(scoreToDisplay(1)).toEqual("15");
+  });
+});*/
+
+describe("Test of game()", function(){
+  it("Should render increase one players score", function() {
+    const game = new Game()
+    expect(game.scoreToDisplay()).toEqual("love-love");
+  });
+});
+
+describe("Test of game()", function(){
+  it("Should render increase one players score", function() {
+    const game = new Game().score("Player1")
+    expect(game.scoreToDisplay()).toEqual("15-love");
+  });
+});
+
+describe("Test of game()", function(){
+  it("Should render 0 point for Player1 and one point for Player2", function() {
+    const game = new Game().score("Player2")
+    expect(game.scoreToDisplay()).toEqual("love-15");
+  });
+});
+
+describe("Test of game()", function(){
+  it("Should render 1-1 ", function() {
+    const game = new Game()
+    .score("Player2")
+    .score("Player1")
+    expect(game.scoreToDisplay()).toEqual("15-15");
+  });
+});
+
+describe("Test of game()", function(){
+  it("Should render deuce", function() {
+    const game = new Game()
+    .score("Player2")
+    .score("Player2")
+    .score("Player2")
+    .score("Player1")
+    .score("Player1")
+    .score("Player1")
+    expect(game.scoreToDisplay()).toEqual("Deuce");
+  });
+});
+
+describe("Test of game()", function(){
+  it("Should render advantage for one player", function() {
+    const game = new Game()
+    .score("Player2")
+    .score("Player2")
+    .score("Player2")
+    .score("Player1")
+    .score("Player1")
+    .score("Player1")
+    .score("Player1")
+    expect(game.scoreToDisplay()).toEqual("Advantage for Player1");
+  });
+});
+
+describe("Test of game()", function(){
+  it("Should render game won for Player1", function() {
+    const game = new Game()
+    .score("Player2")
+    .score("Player2")
+    .score("Player2")
+    .score("Player1")
+    .score("Player1")
+    .score("Player1")
+    .score("Player1")
+    .score("Player1")
+    expect(game.scoreToDisplay()).toEqual("The winner is Player1");
+  });
+});
+
+describe("Test of game()", function(){
+  it("Should render deuce after one advantage for Player1", function() {
+    const game = new Game()
+    .score("Player2")
+    .score("Player2")
+    .score("Player2")
+    .score("Player1")
+    .score("Player1")
+    .score("Player1")
+    .score("Player1")
+    .score("Player2")
+    expect(game.scoreToDisplay()).toEqual("Deuce");
+  });
+});
+
+describe("Test of game()", function(){
+  it("Should render one win for Player2 after 4 points in a row", function() {
+    const game = new Game()
+    .score("Player2")
+    .score("Player2")
+    .score("Player2")
+    .score("Player2")
+    expect(game.scoreToDisplay()).toEqual("The winner is Player2");
+  });
+});
+
+/*describe("Test of game()", function(){
+  it("Should render an error if we score after winning", function() {
+    const game = new Game()
+    .score("Player2")
+    .score("Player2")
+    .score("Player2")
+    .score("Player2")
+    expect(game.score("Player2")).toThrow('Game ended');
+  });
+});*/
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,116 @@
-export function systemUnderTest(): boolean {
-  return true;
+type Player = "Player1" | "Player2"
+
+type Score = 0 | 1 | 2 | 3
+
+type Points = Record<Player, Score>;
+
+const initPoints: Points = {
+  Player1: 0,
+  Player2: 0
+};
+
+const displayPoints: Record<Score, string> = {
+  0: "love",
+  1: "15",
+  2: "30",
+  3: "40",
+};
+
+export const decidePlayerToScore = ():number => {
+  return Math.floor(Math.random() * (2 - 1 + 1)) + 1
 }
+
+export const otherPlayer = (player: Player):Player => {
+  if(player === "Player1")
+    return "Player2"
+  return "Player1"
+}
+
+/*export const scoreToDisplay = (pastScore: Score = 0) => {
+  return displayPoints[pastScore]
+}*/
+
+interface GameInterface {
+  score(player:Player):GameInterface,
+  scoreToDisplay():string
+}
+
+export class GameWon implements GameInterface {
+  constructor(private winner: Player) {}
+
+  score(): GameInterface {
+      throw ("Game ended");
+  }
+
+  scoreToDisplay(): string {
+      return `The winner is ${this.winner}`
+  }
+
+}
+
+export class Advantage implements GameInterface {
+  constructor(private advantagedPlayer: Player) {}
+
+  score(player: Player): GameInterface {
+    return player === this.advantagedPlayer ? new GameWon(player) : new Deuce();
+  }
+
+  scoreToDisplay(): string {
+      return `Advantage for ${this.advantagedPlayer}`
+  }
+}
+
+export class Deuce implements GameInterface {
+  constructor() {}
+
+  score(player: Player): GameInterface {
+    return new Advantage(player);
+  }
+
+  scoreToDisplay(): string {
+      return `Deuce`
+  }
+}
+
+export class Game implements GameInterface {
+  constructor(private points: Points = initPoints) {}
+
+  score = (player:Player) => {
+
+    const other = otherPlayer(player)
+
+    //If player wins
+    if(this.points[player] === 3 )
+      return new GameWon(player)
+    
+    if(this.points[player] == 2 && this.points[other] === 3)
+      return new Deuce()
+
+    return new Game({
+      ...this.points,
+      [player]: this.points[player] + 1
+    });
+
+  }
+
+  scoreToDisplay = () => {
+    return `${displayPoints[this.points.Player1]}-${displayPoints[this.points.Player2]}`
+  }
+
+}
+
+
+//console.log(game.scoreToDisplay())
+let game = new Game()
+//console.log(game = new Game().score('Player1').score('Player2').scoreToDisplay())
+
+console.log(
+  game
+  .score('Player1')
+  .score('Player2')
+  .score('Player2')
+  .score('Player2')
+  .score('Player2')
+  .score('Player2')
+  .scoreToDisplay()
+)
